0a2b1b20bc92d3b92eb95024fa24e25d69d1fd97
DuyCoder <duynv.it.052@gmail.com>
2024-09-30 08:46:10 +0700

    update-security

---
+++ src/main/java/com/rent/game/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.rent.game.config;
+
+import com.rent.game.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public JwtAuthFilter authFilter() {
+        return new JwtAuthFilter();
+    }
+}
---
+++ src/main/java/com/rent/game/service/JwtService.java
@@ -0,0 +1,7 @@
+package com.rent.game.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+}
